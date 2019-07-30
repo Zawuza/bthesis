401119094587bba32f9009cbe5136cae29103312
Zawuza <andreizawuza@gmail.com>
2019-07-30 13:33:54 +0200

    More datasets: - Fixes for 25x25uniform - Scripts for 25x25mallos[0_2] - Database update

--- data_gen/l_25x25_uniform/25x25uniform.py
+++ data_gen/l_25x25_uniform/25x25uniform.py
@@ -2,10 +2,13 @@ import random
 
 alternatives = [str(i) for i in range(25)]
 
+output = ""
 for i in range(200):
     profile = []
     for j in range(25):
         random.shuffle(alternatives)
         v = alternatives.copy()
         profile.append(v)
-    print('("' + str(profile) + '"),')
+    output += '("' + str(profile) + '"),'
+with open("data_gen\\l_25x25_uniform\\25x25uniform.sql", "w") as text_file:
+    text_file.write(output)
